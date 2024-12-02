1181a25f770097fa318f3f7ed1c7640e70d2a02a
artemgon <artemhoncharuk123@gmail.com>
2024-12-02 17:22:13 +0200

    Task 1 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,33 @@
 #include "libs.h"
+
+class Student
+{
+protected:
+	string _name, _surname, _stage;
+	int _age;
+public:
+	Student(const string& name, const string& surname, const string& stage, int age)
+	: _name(name), _surname(surname), _stage(stage), _age(age) {}
+	virtual void about() const
+	{
+		cout << "Name: " << _name << endl;
+		cout << "Surname: " << _surname << endl;
+		cout << "Stage: " << _stage << endl;
+		cout << "Age: " << _age << endl;
+	}
+	~Student() = default;
+};
+
+class Aspirant : public Student
+{
+public:
+	Aspirant(const string& name, const string& surname, int age) : Student(name, surname, "0", age) {}
+	void about() const override
+	{
+		cout << "Name: " << _name << endl;
+		cout << "Surname: " << _surname << endl;
+		cout << "Stage: " << _stage << endl;
+		cout << "Age: " << _age << endl;
+	}
+	~Aspirant() = default; 
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "classes.h"
 
 int main()
-{
+{	
+	Student student1("Kiril", "Adamovich", "2 year", 19);
+	Aspirant aspirant1("Petro", "Kohut", 17);
+	student1.about();
+	aspirant1.about();
 	return 0;
 }
