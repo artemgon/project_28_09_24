43e2e6180c2e20a387d496859852c3219dcc7d27
artemgon <artemhoncharuk123@gmail.com>
2024-12-06 13:26:08 +0200

    Task 2 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,32 @@
 #include "libs.h"
+
+class Passport
+{
+protected:
+	string _snp, _city, _sex, _birth_date, _number;
+public:
+	Passport(string snp, string city, string sex, string birth_date, string number) : _snp(snp), _city(city), _sex(sex), _birth_date(birth_date), _number(number) {}
+	virtual void printFunc() const
+	{
+		cout << "SNP: " << _snp << endl;
+		cout << "City: " << _city << endl;
+		cout << "Sex(M/F): " << _sex << endl;
+		cout << "Birth date: " << _birth_date << endl;
+		cout << "Number: " << _number << endl;
+	}
+	~Passport() = default;
+};
+
+class ForeignPassport : public Passport
+{
+protected:
+	string _visa_data;
+public:
+	ForeignPassport(string snp, string city, string sex, string birth_date, string number, string visa_data) : Passport(snp, city, sex, birth_date, number), _visa_data(visa_data) {}
+	void printFunc() const override
+	{
+		Passport::printFunc();
+		cout << "Visa data: " << _visa_data << endl;
+	}
+	~ForeignPassport() = default;
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+	Passport p1("Ivanov Ivan Ivanovich", "Kyiv", "M", "01.01.2000", "3465234");
+	p1.printFunc();
+	cout << endl;	
+	ForeignPassport p2("Petrov Petr Petrovich", "Lviv", "M", "02.02.2000", "1234567890", "01.01.2020 - 01.01.2021");
+	p2.printFunc();
 	return 0;
 }
