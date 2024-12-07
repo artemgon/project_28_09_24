606c26db5093d6ff48aa22405a8e0a0347d3f204
artemgon <artemhoncharuk123@gmail.com>
2024-12-07 20:15:24 +0200

    Task 2 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,74 @@
-#include "libs.h"
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "libs.h"1
+
+class MyString
+{
+protected:
+	char _str[100] = {};
+public:
+	MyString()
+	{
+		_str[0] = '\0';
+	}
+	MyString(const char s[], int len)
+	{
+		strncpy(_str, s, len);
+		_str[len] = '\0';
+	}
+	MyString& operator=(const MyString& s) {
+		strncpy(_str, s._str, 99);
+		_str[99] = '\0'; 
+		return *this;
+	}
+	int lengthFunc()
+	{
+		return strlen(_str);
+	}
+	const char* getStr() const { return _str; }
+	void clearFunc()
+	{
+		_str[0] = '\0';
+	}
+	MyString operator+(MyString& s) const
+	{
+		MyString temp;
+		strncpy(temp._str, _str, 99);
+		strncat(temp._str, s._str, 99 - strlen(temp._str));
+		return temp;
+	}
+	MyString& operator+=(const MyString& s) {
+		strncat(_str, s._str, 99 - strlen(_str));
+		return *this;
+	}
+	bool operator==(MyString& s)
+	{
+		return strcmp(_str, s._str) == 0;
+	}
+	bool operator!=(MyString& s)
+	{
+		return strcmp(_str, s._str) != 0;
+	}
+	virtual ~MyString() = default;
+};
+class MyBitString : public MyString
+{
+public:
+	MyBitString() : MyString() {}
+	MyBitString(const char s[], int len) {
+		for (int i = 0; i < len; i++) {
+			if (s[i] != '0' && s[i] != '1') {
+				clearFunc();
+				return;
+			}
+		}
+		strncpy(const_cast<char*>(getStr()), s, len);
+	}
+
+	void changeFunc() {
+		char* str = const_cast<char*>(getStr());
+		for (int i = 0; i < strlen(str); i++) {
+			str[i] = (str[i] == '0') ? '1' : '0';
+		}
+	}
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 
 int main()
 {
+	MyString s("Hello ", 6);
+	cout << "s: " << s.getStr() << endl;
+	MyString s2("World", 5);
+	cout << "s2: " << s2.getStr() << endl;
+	s += s2;
+	cout << "s after += s2: " << s.getStr() << endl;
+	MyString s3(". It's me", 9);
+	cout << "s3: " << s3.getStr() << endl;
+	s = s + s3;
+	cout << "s after = s + s3: " << s.getStr() << endl;
+	cout << "s length: " << s.lengthFunc() << endl;
+	cout << "s == s2: " << (s == s2) << endl;
+	cout << "s != s2: " << (s != s2) << endl;
+	s.clearFunc();
+	cout << "s after clear: " << s.getStr() << endl;
+    MyBitString s4("1101", 4);
+    MyBitString s5("1010", 4);
+    cout << "s1: " << s4.getStr() << endl;
+    cout << "s2: " << s5.getStr() << endl;
+    s4.changeFunc();
+    cout << "s1 after change: " << s4.getStr() << endl;
+    s4.clearFunc();
+    cout << "s1 after clear: " << s5.getStr() << endl;
 	return 0;
 }
