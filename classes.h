afd385f05b267e9fcb454a58c5f5f38d60faa516
artemgon <artemhoncharuk123@gmail.com>
2024-12-07 13:49:08 +0200

    Task 3 is done. PE is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,46 @@
 #include "libs.h"
+
+class Transport
+{
+public:
+	virtual float _timeFunc() const = 0;
+	virtual float _fareFunc() const = 0;
+	virtual void printFunc() const = 0;
+	virtual ~Transport() = default;
+};
+
+class Car : public Transport
+{
+private:
+	float _time;
+	float _fare;
+public:
+	Car(float time, float fare) : _time(time), _fare(fare) {}
+	float _timeFunc() const override { return _time; }
+	float _fareFunc() const override { return _fare; }
+	void printFunc() const override { cout << "Car: _time = " << _time << ", _fare = " << _fare << endl; }
+};
+
+class Bicycle : public Transport
+{
+private:
+	float _time;
+	float _fare;
+public:
+	Bicycle(float time, float fare) : _time(time), _fare(fare) {}
+	float _timeFunc() const override { return _time; }
+	float _fareFunc() const override { return _fare; }
+	void printFunc() const override { cout << "Bicycle: _time = " << _time << ", _fare = " << _fare << endl; }
+};
+
+class Cart : public Transport
+{
+private:
+	float _time;
+	float _fare;
+public:
+	Cart(float time, float fare) : _time(time), _fare(fare) {}
+	float _timeFunc() const override { return _time; }
+	float _fareFunc() const override { return _fare; }
+	void printFunc() const override { cout << "Cart: _time = " << _time << ", _fare = " << _fare << endl; }
+};	
--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
+	Transport* transport[3];
+	transport[0] = new Car(1.5, 100);
+	transport[1] = new Bicycle(2.5, 50);
+	transport[2] = new Cart(3.5, 25);
+	transport[0]->printFunc();
+	transport[1]->printFunc();
+	transport[2]->printFunc();
 	return 0;
 }
