cdd5622c36109646b8a0cf4e70975f5d3efb7395
artemgon <artemhoncharuk123@gmail.com>
2024-12-07 13:55:52 +0200

    Task 1 is done.

--- classes.h
+++ classes.h
@@ -1,1 +1,51 @@
 #include "libs.h"
+
+class Pet
+{
+protected:
+	string name, characteristics;
+public:
+	Pet(string name, string characteristics) : name(name), characteristics(characteristics) {}
+	virtual void printFunc()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Characteristics: " << characteristics << endl;
+	}
+	~Pet() = default;
+};
+
+class Dog : public Pet
+{
+public:
+	Dog(string name, string characteristics) : Pet(name, characteristics) {}
+	void printFunc() override
+	{
+		cout << "Dog" << endl;
+		Pet::printFunc();
+	}
+	~Dog() = default;
+};
+
+class Cat : public Pet
+{
+public:
+	Cat(string name, string characteristics) : Pet(name, characteristics) {}
+	void printFunc() override
+	{
+		cout << "Cat" << endl;
+		Pet::printFunc();
+	}
+	~Cat() = default;
+};
+
+class Parrot : public Pet
+{
+public:
+	Parrot(string name, string characteristics) : Pet(name, characteristics) {}
+	void printFunc() override
+	{
+		cout << "Parrot" << endl;
+		Pet::printFunc();
+	}
+	~Parrot() = default;
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
+	Dog dog("Arnold", "Age:5 , weight: 35 kg, height: 60 cm");
+	Cat cat("Tom", "Age:3 , weight: 5 kg, height: 25 cm");
+	Parrot parrot("Kesha", "Age:2 , weight: 0.5 kg, height: 15 cm");
+	dog.printFunc();
+	cat.printFunc();
+	parrot.printFunc();
 	return 0;
 }
